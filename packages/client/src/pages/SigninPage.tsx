3100378a444a4eacf6575161205484d95b3d3146
andreissh <55095154+andreissh@users.noreply.github.com>
2025-08-27 10:54:24 +0300

    9-signin-page-layout | Верстка страницы логина (#25)

--- packages/client/src/pages/SigninPage.tsx
+++ packages/client/src/pages/SigninPage.tsx
@@ -1,21 +1,32 @@
 import { Helmet } from 'react-helmet'
 import { PageInitArgs } from '../routes'
 import styled from 'styled-components'
+import { PageContainer } from '../styles/PageContainer'
+import { Form } from '../styles/Form'
+import { Input } from '../styles/Input'
+import { Button } from '../styles/Buttons'
 
 export const SigninPage = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
+    <>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Вход</title>
         <meta name="description" content="Страница логина" />
       </Helmet>
-      <SigninForm>
-        <input type="text" placeholder="Логин" />
-        <input type="password" placeholder="Пароль" />
-        <button type="submit">Войти</button>
-      </SigninForm>
-    </div>
+      <PageContainer>
+        <Form onSubmit={handleSubmit}>
+          <Header className="font-secondary">ВХОД</Header>
+          <Input id="login" type="text" placeholder="Логин" />
+          <Input id="password" type="password" placeholder="Пароль" />
+          <Button type="submit">ВОЙТИ</Button>
+        </Form>
+      </PageContainer>
+    </>
   )
 }
 
@@ -23,15 +34,9 @@ export const initSigninPage = async (_args: PageInitArgs) => {
   return Promise.resolve()
 }
 
-const SigninForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  max-width: 300px;
-  margin: 0 auto;
-
-  input,
-  button {
-    padding: 0.5rem;
-  }
+const Header = styled.h1`
+  text-align: center;
+  color: var(--color-primary);
+  font-size: 64px;
+  line-height: 64px;
 `
