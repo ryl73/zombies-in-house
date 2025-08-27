da98d30fc996a9d1c3bdffab69900b8da0a3a3b4
andreissh <55095154+andreissh@users.noreply.github.com>
2025-08-27 18:56:40 +0300

    13-signup-layout | Верстка страницы регистрации (#26)

--- packages/client/src/styles/PageContainer.ts
+++ packages/client/src/styles/PageContainer.ts
@@ -4,6 +4,7 @@ export const PageContainer = styled.div`
   min-height: 100vh;
   background-color: var(--color-bg-primary);
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `
---
+++ packages/client/src/styles/ThemedHeader.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const ThemedHeader = styled.h1`
+  font-family: 'Rubik Wet Paint', cursive;
+  text-align: center;
+  color: var(--color-primary);
+  font-size: 64px;
+  line-height: 64px;
+`
