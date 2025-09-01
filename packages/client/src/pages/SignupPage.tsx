3456e89c0012963554b44d6f0b2f7b1ddf79ae20
andreissh <55095154+andreissh@users.noreply.github.com>
2025-09-01 20:53:53 +0300

    5.12 | Добавление валидации форм логина и регистрации (#27)

--- packages/client/src/pages/SignupPage.tsx
+++ packages/client/src/pages/SignupPage.tsx
@@ -1,14 +1,58 @@
 import { Helmet } from 'react-helmet'
 import { PageInitArgs } from '../routes'
-import { Form } from '../styles/Form'
 import { PageContainer } from '../styles/PageContainer'
+import { Form as StyledForm } from '../styles/Form'
 import { Input } from '../styles/Input'
 import { Button } from '../styles/Buttons'
 import { ThemedHeader } from '../styles/ThemedHeader'
+import { ErrorMessage } from '../styles/Errors'
+import { Formik, Field } from 'formik'
+import * as Yup from 'yup'
+import { validation } from '../utils/validation'
+
+type FormValues = {
+  login: string
+  email: string
+  first_name: string
+  second_name: string
+  phone: string
+  password: string
+  password_repeat: string
+}
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string()
+    .matches(validation.email.pattern, validation.email.message)
+    .required('Почта обязательна'),
+  login: Yup.string()
+    .min(3, 'Логин должен быть не менее 3 символов')
+    .max(20, 'Логин должен быть не более 20 символов')
+    .matches(validation.login.pattern, validation.login.message)
+    .required('Логин обязателен'),
+  first_name: Yup.string()
+    .matches(validation.name.pattern, validation.name.message)
+    .required('Имя обязательно'),
+  second_name: Yup.string()
+    .matches(validation.name.pattern, validation.name.message)
+    .required('Фамилия обязательна'),
+  phone: Yup.string()
+    .min(10, 'Номер телефона должен содержать не менее 10 цифр')
+    .max(15, 'Номер телефона не должен быть более 15 цифр')
+    .matches(validation.phone.pattern, validation.phone.message)
+    .required('Телефон обязателен'),
+  password: Yup.string()
+    .min(8, 'Пароль должен быть не менее 8 символов')
+    .max(40, 'Пароль должен быть не более 40 символов')
+    .matches(validation.password.pattern, validation.password.message)
+    .required('Пароль обязателен'),
+  password_repeat: Yup.string()
+    .oneOf([Yup.ref('password')], 'Пароли должны совпадать')
+    .required('Повторите пароль'),
+})
 
 export const SignupPage = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const onSubmit = (values: FormValues) => {
+    console.log(values)
   }
 
   return (
@@ -20,20 +64,69 @@ export const SignupPage = () => {
       </Helmet>
       <PageContainer>
         <ThemedHeader>РЕГИСТРАЦИЯ</ThemedHeader>
-        <Form onSubmit={handleSubmit}>
-          <Input id="email" type="text" placeholder="Почта" />
-          <Input id="login" type="text" placeholder="Логин" />
-          <Input id="first_name" type="text" placeholder="Имя" />
-          <Input id="second_name" type="text" placeholder="Фамилия" />
-          <Input id="phone" type="text" placeholder="Телефон" />
-          <Input id="password" type="password" placeholder="Пароль" />
-          <Input
-            id="password_repeat"
-            type="password"
-            placeholder="Повторите пароль"
-          />
-          <Button type="submit">Войти</Button>
-        </Form>
+        <Formik
+          initialValues={{
+            email: '',
+            login: '',
+            first_name: '',
+            second_name: '',
+            phone: '',
+            password: '',
+            password_repeat: '',
+          }}
+          validationSchema={SignupSchema}
+          onSubmit={onSubmit}>
+          {({ errors, touched, handleSubmit }) => (
+            <StyledForm onSubmit={handleSubmit}>
+              <Field as={Input} name="email" placeholder="Почта" />
+              {errors.email && touched.email && (
+                <ErrorMessage>{errors.email}</ErrorMessage>
+              )}
+
+              <Field as={Input} name="login" placeholder="Логин" />
+              {errors.login && touched.login && (
+                <ErrorMessage>{errors.login}</ErrorMessage>
+              )}
+
+              <Field as={Input} name="first_name" placeholder="Имя" />
+              {errors.first_name && touched.first_name && (
+                <ErrorMessage>{errors.first_name}</ErrorMessage>
+              )}
+
+              <Field as={Input} name="second_name" placeholder="Фамилия" />
+              {errors.second_name && touched.second_name && (
+                <ErrorMessage>{errors.second_name}</ErrorMessage>
+              )}
+
+              <Field as={Input} name="phone" placeholder="Телефон" />
+              {errors.phone && touched.phone && (
+                <ErrorMessage>{errors.phone}</ErrorMessage>
+              )}
+
+              <Field
+                as={Input}
+                name="password"
+                type="password"
+                placeholder="Пароль"
+              />
+              {errors.password && touched.password && (
+                <ErrorMessage>{errors.password}</ErrorMessage>
+              )}
+
+              <Field
+                as={Input}
+                name="password_repeat"
+                type="password"
+                placeholder="Повторите пароль"
+              />
+              {errors.password_repeat && touched.password_repeat && (
+                <ErrorMessage>{errors.password_repeat}</ErrorMessage>
+              )}
+
+              <Button type="submit">Зарегистрироваться</Button>
+            </StyledForm>
+          )}
+        </Formik>
       </PageContainer>
     </div>
   )
