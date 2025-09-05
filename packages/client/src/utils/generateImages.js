f7fbc3b983007b2cdfaa3c9e482be54262c2fd78
andreissh <shkudov.and@gmail.com>
2025-09-05 22:22:26 +0300

    fixes

--- packages/client/src/utils/generateImages.js
+++ packages/client/src/utils/generateImages.js
@@ -1,9 +1,9 @@
-import { glob } from 'glob'
+import fg from 'fast-glob'
 import { promises as fs } from 'fs'
 import path from 'path'
 
 const imagesDir = path.join(process.cwd(), 'dist/client/images')
-const files = await glob('**/*.*', { cwd: imagesDir, absolute: false })
+const files = await fg('**/*.*', { cwd: imagesDir, absolute: false })
 
 const urls = files.map(f => '/images/' + f.replace(/\\/g, '/'))
 
