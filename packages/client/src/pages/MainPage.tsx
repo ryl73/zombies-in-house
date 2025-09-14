9979e26279b555ab0108a84fe8627b2691c01251
MarsiKris76 <bykovme@krista.ru>
2025-09-14 08:03:19 +0300

    Реализован хук для проверки авторизации.

--- packages/client/src/pages/MainPage.tsx
+++ packages/client/src/pages/MainPage.tsx
@@ -17,10 +17,8 @@ import {
 import { makeStyles } from '@material-ui/core/styles'
 import { logout } from '../api/LoginAPI'
 import { useNavigate } from 'react-router-dom'
-import { clearUser, isUserLoggedIn } from '../slices/userSlice'
-import { useDispatch } from 'react-redux'
-import { useAppSelector } from '../hooks/useApp'
 import { FullscreenToggle } from '../components/FullscreenToggle/FullscreenToggle'
+import { useAuth } from '../hooks/useAuth'
 
 const useStyles = makeStyles(theme => ({
   firstScreen: {
@@ -68,8 +66,7 @@ const useStyles = makeStyles(theme => ({
 
 export const MainPage = () => {
   usePage({ initPage: initMainPage })
-  const dispatch = useDispatch()
-  const isLoggedIn = useAppSelector(isUserLoggedIn)
+  const { isLoggedIn, clearUser } = useAuth()
   const navigate = useNavigate()
   const theme = useTheme()
   const classes = useStyles()
@@ -205,7 +202,7 @@ export const MainPage = () => {
           <Button
             component={Link}
             onClick={() => {
-              logout().then(() => dispatch(clearUser()))
+              logout().then(() => clearUser())
             }}
             to="/signin"
             variant="contained"
